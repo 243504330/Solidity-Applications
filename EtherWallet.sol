037a080deb7e64caaf161c0ad9b0dd6916fabba0
243504330 <243504330@qq.com>
2022-03-12 14:58:17 +0800

    一个可以接收以太坊的合约
    receive() external payable {}

---
+++ EtherWallet.sol
@@ -0,0 +1,27 @@
+// 以太钱包
+// 一个基本钱包的例子。
+
+// 任何人都可以发送 ETH。
+// 只有所有者可以撤回。
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner, "caller is not owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
